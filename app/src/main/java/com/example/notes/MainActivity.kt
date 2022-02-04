7da72cd478b91ebe627345a887a0369a379a6924
Stephenayor <40307603+Stephenayor@users.noreply.github.com>
2022-02-04 13:15:04 +0200

    Navigation drawer added

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.notes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+            binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        setupNavigation()
+        }
+
+        private fun setupNavigation(){
+            val navController = findNavController(R.id.nav_host_fragment)
+            setupActionBarWithNavController(navController,binding.drawerLayout)
+            binding.navView.setupWithNavController(navController)
+        }
+
+        override fun onSupportNavigateUp(): Boolean {
+            return NavigationUI.navigateUp(findNavController(R.id.nav_host_fragment),binding.drawerLayout)
+        }
     }
-}
--- app/src/main/java/com/example/notes/NoteListFragment.kt
+++ app/src/main/java/com/example/notes/NoteListFragment.kt
@@ -1,19 +1,21 @@
 package com.example.notes
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.example.notes.databinding.FragmentNoteListBinding
+import com.google.android.material.navigation.NavigationView
 
 
 class NoteListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var actionBarToggle: ActionBarDrawerToggle
+    private lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,8 +28,15 @@ class NoteListFragment : Fragment() {
         inflater.inflate(R.layout.fragment_note_list, container, false)
         val binding = DataBindingUtil.inflate<FragmentNoteListBinding>(inflater, R.layout.fragment_note_list, container,
             false)
+//
+//
+//        binding.noteFragment = NoteListFragment()
+//        var drawerLayout2: DrawerLayout = binding.drawerLayout
+//        actionBarToggle = ActionBarDrawerToggle(activity, drawerLayout2, 0, 0)
+//        drawerLayout.addDrawerListener(actionBarToggle)
+//        activity?.actionBar?.setDisplayHomeAsUpEnabled(true)
+//        actionBarToggle.syncState()
         return binding.root
-
     }
 
 }
